84ba9ac955cf4566296717606fdddb5dd1846442
dannyl <danilovlaurent@pursuit.org>
2019-10-21 20:38:09 -0400

    Question 2

--- classeslab.js
+++ classeslab.js
@@ -14,5 +14,30 @@ class Person{
 let person1 = new Person("Cassidy", "Beni", "Ena");
 let person2 = new Person("Danny", "Laurent", "Darnell");
 //console.log(person1.firstName)
-console.log(person1.fullName())
+// console.log(person1.fullName())
+
+
+// QUESTION 2
+
+
+class Book {
+    constructor(title,author,rating){
+        this.title = title
+        this.author = author
+        this.rating = rating
+    }
+    isGood(rating){
+     if (rating > 7){
+        return true
+    } else{
+        return false
+    }
+
+    }
+}
+
+let book1 = new Book("Harry Poter"," J.K Rowling",5)
+let book2 = new Book("A Scarlet Letter", "Nathaniel Hawthhorne", 8)
+
+// console.log(book1.isGood(book1.rating))
 
